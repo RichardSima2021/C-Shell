535c9c4e1ec801e2d0c24ced56cc77ae6d13742a
Richard Sima <Richard.Sima2021@gmail.com>
2023-10-15 15:23:41 -0700

    feat: included headers, hello world

---
+++ hw2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <ctype.h>
+#include <signal.h>
+#include <fcntl.h>
+
+int main(){
+    printf("Hello World\n");
+}
