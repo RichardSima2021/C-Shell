fe09c98ca50ddb8f78823f550d63a06af1428e8a
Richard Sima <Richard.Sima2021@gmail.com>
2023-10-15 20:05:39 -0700

    feat: quit command implemented and can recognize arguments

--- hw2.c
+++ hw2.c
@@ -9,6 +9,59 @@
 #include <signal.h>
 #include <fcntl.h>
 
+#define bufferSize 80
+#define whitespace " \t\n"
+#define quit "quit"
+
+int debug = 1;
+
+void promptInput(char* inputBuffPtr){
+    printf("%s", "prompt > ");
+    fgets(inputBuffPtr, bufferSize, stdin);
+}
+
 int main(){
-    printf("Hello World\n");
+
+    char* command = "";
+    
+    while(1){
+        char inputBuffer[bufferSize]; 
+        promptInput(inputBuffer);
+        command = strtok(inputBuffer, whitespace);
+        
+        if(strcmp(command, quit) == 0){
+            break;
+        } else{
+            if (debug) printf("%s\n", command); 
+            if (debug) printf("args: ");
+
+            // read first argument if exists
+            char* arg = strtok(NULL, whitespace); // arg is a pointer to the first cell in a char array
+            int argpos = 0;
+            char* args[80] = {NULL}; // defines an array of 80 charpointers, these will store all the arguments of the command if there are any
+            
+
+            while(arg != NULL){
+                args[argpos] = strdup(arg); // pointer to duplicated argument since arg will be overwritten by next strtok call
+                argpos += 1;
+                // continue reading arguments if exists
+                arg = strtok(NULL, whitespace);
+            }
+            if (debug) printf("\nargpos: %d\n", argpos);
+
+            for(int i = 0; i < argpos; i++){
+                printf("%s ", args[i]);
+            }
+
+            if (debug) printf("\n");
+
+
+
+
+        }
+
+    }
+
+    printf("%s\n","exit");
+    return 0;
 }
