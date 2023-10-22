eeb219ce399a41fe8911312f8106ff2258927b60
Richard Sima <Richard.Sima2021@gmail.com>
2023-10-21 17:57:16 -0700

    feat: fg proccess can be stopped, joblist updated to reflect fg process statuses

--- hw2.c
+++ hw2.c
@@ -14,7 +14,7 @@
 #define quit "quit"
 
 
-int debug = 0;
+int debug = 1;
 
 struct Job{
     // each child process owns a struct
@@ -22,7 +22,7 @@ struct Job{
     int processId; // returned to parent
 
     // 1 = fg
-    // 2 = bg
+    // 0 = bg
     int foreground;
     // Running
     // Stopped
@@ -34,29 +34,96 @@ struct Job{
 struct Job* jobList[5] = {NULL};
 int latestJobId = 1;
 int nextJobIndex = 0;
+int foregroundStopped = 0;
+
+void cleanJobList(int endedPid){
+    // removes the job within joblist of the job that just ended
+    // shifts nextJobIndex back by 1
+    // free each argument, then free the argument array, then free the struct
+    int removeIndex;
+    for(int i = 0; i < nextJobIndex; i++){
+        if (jobList[i] -> processId == endedPid){
+            removeIndex = i;
+        }
+    }
+
+    for(int a = 0; a < jobList[removeIndex] -> numargs; a++){
+        free(jobList[removeIndex] -> args[a]); 
+    }
+
+    free(jobList[removeIndex] -> args);
+    free(jobList[removeIndex]);
+    jobList[removeIndex] = NULL;
+
+    for(int i = 0; i < 4; i++){
+        // shift everything in jobList back by 1 after removing a job;
+        if(jobList[i] == NULL && jobList[i+1] != NULL){
+            jobList[i] = jobList[i+1];
+            jobList[i+1] = NULL;
+        }
+    }
+
+    // next available index in jobList goes back by 1
+    nextJobIndex -= 1;
+}
+
+void jobStatusStopped(int stoppedPid){
+    // updates the job with stoppedPid's status to stopped
+    for(int i = 0; i < nextJobIndex; i++){
+        if(jobList[i] -> processId == stoppedPid){
+            jobList[i] -> status = "Stopped";
+            jobList[i] -> foreground = 0;
+            break;
+        }
+    }
+}
 
 void nonResponseINTHandler(int sig){
     if (debug) printf("Handled SIGINT from %d\n", getpid());
 }
+
 void nonResponseTSTPHandler(int sig){
     if (debug) printf("Handled TSTP from %d\n", getpid());
 }
+
+void shellTSTPHandler(int sig){
+    if (debug) printf("Handling TSTP from shell\n");
+}
+
 void SIGCHLDHandler(int sig){
     if (debug) printf("SIGCHLD handler called\n");
     int child_status;
     int waitpid_status = waitpid(-1, &child_status, WNOHANG); 
     if (debug) printf("waitpid called\n");
     if (debug){
-        if(waitpid_status <= 0){
+        if(waitpid_status < 0){
             perror("waitpid");
         } else if(waitpid_status > 0){
-            printf("Child reaped by SIGCHLD handler\n");
+            printf("Child pid: %d stopped or terminated\n", waitpid_status);
+        } else{
+            printf("No children to be reaped\n");
+        }
+    }
+    
+    if(waitpid_status > 0){
+        if (WIFEXITED(child_status)){
+            if (debug) printf("Child finished and reaped\n");
+            // remove from job list
+            cleanJobList(waitpid_status);
+        } else if (WIFSIGNALED(child_status)){
+            if (debug) printf("Child was terminated and reaped\n");
+            // remove from job list
+            cleanJobList(waitpid_status);
+        } else if (WIFSTOPPED(child_status)){
+            if (debug) printf("Child was stopped\n");
         }
     }
 }
+
 void nonResponseCHLDHandler(int sig){
     if (debug) printf("PID: %d encountered a SIGCHLD signal and ignored it\n", getpid());
 } 
+
 
 void promptInput(char* inputBuffPtr){
     printf("%s", "prompt > ");
@@ -98,6 +165,7 @@ void executeTaskFg(char* programName, char** args, int numargs){
     if(pid == 0){
         signal(SIGINT, SIG_DFL); // fg task must be able to be affected by Ctrl+C
         signal(SIGTSTP, SIG_DFL); // fg task must be able to be affected by Ctrl+Z
+        // signal(SIGUSR2, foregroundUSR2Handler);
         // signal(SIGCHLD, nonResponseCHLDHandler); 
         if (debug) printf("Forked child and running fg process: %s pid: %d, pgid: %d\n", programName, getpid(), getpgid(getpid()));
         // child
@@ -106,12 +174,49 @@ void executeTaskFg(char* programName, char** args, int numargs){
             printf("%s: Program not found.\n", programName);
             exit(0);
         }
+        kill(getppid(), SIGUSR1);
     } else{
         // parent, pid = process id of child
         int child_status;
         if (debug) printf("shell waiting for foreground task to complete\n");
-        waitpid(pid, &child_status, 0); 
-        if (debug) printf("Foreground child finished and reaped\n");
+
+        struct Job* newForeroundJob = malloc(sizeof(struct Job)); // will need to free later in SIGCHLD handler or smth
+        newForeroundJob -> jobId = latestJobId;
+        newForeroundJob -> processId = pid;
+        newForeroundJob -> foreground = 1;
+        newForeroundJob -> status = "Running";
+        // newBackgroundJob -> args = args;
+        newForeroundJob -> numargs = numargs;
+
+        if (debug) printf("Duplicating the following arguments: ");
+        char** argsCopy = malloc((numargs + 2) * sizeof(char*));
+        for (int i = 0; i < numargs + 1; i++){
+            if (debug) printf("%s ", args[i]);
+            argsCopy[i] = strdup(args[i]);
+        }
+        argsCopy[numargs + 1] = NULL;
+        newForeroundJob -> args = argsCopy; // will need to free the array and each string later
+
+
+        jobList[nextJobIndex] = newForeroundJob;
+        if (debug) printf("\nCreating a new job and insert into jobList at index %d\n", nextJobIndex);
+        nextJobIndex += 1;
+        latestJobId += 1;
+        
+        int waitpid_status = waitpid(pid, &child_status, WUNTRACED); 
+        
+        if (debug) printf("waitpid of foreground process called\n");
+
+        if (WIFEXITED(child_status)){
+            if (debug) printf("Foreground child finished and reaped\n");
+            cleanJobList(waitpid_status);
+        } else if (WIFSIGNALED(child_status)){
+            if (debug) printf("Foreground child was terminated and reaped\n");
+            cleanJobList(waitpid_status);
+        } else if (WIFSTOPPED(child_status)){
+            if (debug) printf("Foreground child was stopped\n");
+            jobStatusStopped(waitpid_status);
+        }
     }
 }
 
@@ -137,7 +242,7 @@ void executeTaskBg(char* programName, char** args, int numargs){
         struct Job* newBackgroundJob = malloc(sizeof(struct Job)); // will need to free later in SIGCHLD handler or smth
         newBackgroundJob -> jobId = latestJobId;
         newBackgroundJob -> processId = pid;
-        newBackgroundJob -> foreground = 2;
+        newBackgroundJob -> foreground = 0;
         newBackgroundJob -> status = "Running";
         // newBackgroundJob -> args = args;
         newBackgroundJob -> numargs = numargs;
@@ -151,11 +256,19 @@ void executeTaskBg(char* programName, char** args, int numargs){
         argsCopy[numargs + 1] = NULL;
         newBackgroundJob -> args = argsCopy; // will need to free the array and each string later
 
-
         jobList[nextJobIndex] = newBackgroundJob;
         if (debug) printf("\nCreating a new job and insert into jobList at index %d\n", nextJobIndex);
         nextJobIndex += 1;
         latestJobId += 1;
+    }
+}
+
+void killJob(char** args, int numargs){
+    int passedByJobID;
+    if (args[1][0] == '%'){
+        printf("Killing job with jobid: %s", args[1]);
+    } else{
+        printf("Killing job with processid: %s", args[1]);
     }
 }
 
@@ -193,6 +306,11 @@ void executeCommand(char* command, char** args, int numargs){
     } else if(strcmp(command, "kill") == 0){
         // kill job and reap | use kill() syscall with -9 signal to kill process
         // same identifiers
+        if(numargs == 0){
+            printf("Error: no jobid or processid provided");
+        } else {
+            killJob(args, numargs);
+        }
     } else if(strcmp(command, "jobs") == 0){
         // print all jobs from jobList
         int jobIndex = 0;
@@ -220,7 +338,7 @@ void executeCommand(char* command, char** args, int numargs){
 int main(){
     if (debug) printf("shell started in gpid: %d\n", getpgid(getpid()));
     signal(SIGINT, nonResponseINTHandler);
-    signal(SIGTSTP, nonResponseTSTPHandler);
+    signal(SIGTSTP, shellTSTPHandler);
     signal(SIGCHLD, SIGCHLDHandler);
     char* command = "";
     
